24b838b9d103315ca54b1c2e60c6153c0ec1b20a
MVDNS <a.s.meleshenkov@gmail.com>
2024-06-22 23:53:36 +0300

    render update build2

--- routes/v2/productRouter.js
+++ routes/v2/productRouter.js
@@ -5,5 +5,7 @@ const productController = require('../../controller/v2/productController')
 router.get('/', productController.allProductData)
 router.post('/', productController.addProduct)
 router.put('/', productController.editProduct)
+router.post('/category', productController.createCategory)
+router.post('/all', productController.createAll)
 
 module.exports = router
